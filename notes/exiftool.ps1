6b35f114a702b2524c4154cba53eddfbe3a3082a
masonperdue <220426478+masonperdue@users.noreply.github.com>
2026-08-22 12:44:13 -0700

    Added exiftool note

---
+++ notes/exiftool.ps1
@@ -0,0 +1,6 @@
+# Rename Music
+exiftool -v '-filename<Unknown Artist - Untitled - ${Filename}.%e' '-filename<${Artist;s!/!_!g} - Untitled.%e' '-filename<Unknown Artist - ${Title;s!/!_!g}.%e' '-filename<${Artist;s!/!_!g} - ${Title;s!/!_!g}.%e' *.m4a
+
+# Rename pictures
+exiftool -v '-FileName<CreateDate' -d '%Y%m%d-%H%M%S%%-03.c.%%e' .
+
