890440587fd79134613c5bdbf74133a3c85d071d
masonperdue <220426478+masonperdue@users.noreply.github.com>
2026-08-23 12:57:14 -0700

    Fix prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,6 @@ function prompt {
     Write-Host "$esc[36m$user$esc[94m@$hostName$esc[93m:$pwdPath$esc[0m " -NoNewline
     Write-Host ""
     
-    return "$esc[31m>$esc[0m "
+    return "  $esc[31m>$esc[0m"
 }
 
